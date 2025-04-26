192322a0a95e3c368fd3064c14622719b6ed190c
ToothBrush <ang.gi.mo.cci1234@gmail.com>
2025-04-26 13:24:24 +0900

    Initial commit

---
+++ src/main/kotlin/org/ToothBrush/publishTest/PublishTest.kt
@@ -0,0 +1,14 @@
+package org.ToothBrush.publishTest
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class PublishTest : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
